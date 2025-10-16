c4600fa68f81f2f81c9e35e6b22edf4082204379
baam12 <leehyobeom12@gmail.com>
2025-10-17 00:08:38 +0900

    feat(tokenizer): 쉼표/콜론 구분자 및 빈 문자열 처리 추가
    - 입력의 값이 ""로 들어올 경우 결과의 내용을 0으로 출력하도록 구현
    
    - 기본 구분자(쉼표,콜론)을 기준으로 문자열을 분리하여 Strong 타입의 숫자 배열 생성
    
    - 문자열을 정수로 형변환 후 반복문으로 합계를 계산하도록 구현
    
    - 결과 출력 포맷 '결과 : 출력값' 형식으로 구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,27 @@
 package calculator;
 
-
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println(input);
-        System.out.println("결과 : " + input);
+        if(input == null || input.isEmpty()) {
+            String inputBlank = "0";
+            System.out.println("결과 : " + inputBlank);
+        } else {
+            String[] inputSplit = input.split("[,:]");
+
+            System.out.println(Arrays.toString(inputSplit));
+
+            int sum = 0;
+            for (String p : inputSplit) {
+                sum += Integer.parseInt(p);
+            }
+
+            System.out.println("결과 : " + sum);
+        }
     }
 }
